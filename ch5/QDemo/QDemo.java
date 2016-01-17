dca98218e0854470fe540f327fc8bb710404daf1
Emmanuel Goldstein <emmgold@mail.ru>
2016-01-17 17:16:36 +0500

    добавил комментарии в циклическую очередь

--- ch5/QDemo/QDemo.java
+++ ch5/QDemo/QDemo.java
@@ -1,35 +1,40 @@
 // Класс, реализующий очередь для хранения символов
+// дополнил, теперь это циклическая очередь
 
 class Queue {
 	char q[];
 	int putloc, getloc;
 
 	Queue (int size) {
-		q = new char[size+1];
-		putloc = getloc = 0;
-	}
+		q = new char[size+1];	// создаём массив на 1 больше нужного, чтобы обработать
+		putloc = getloc = 0;	// начальный момент, когда начало и конец указывают 
+	}							// на один и тот же элемент
 
-	void put(char ch) {
+	void put(char ch) {	// засовываем новый элемент в циклическую очередь
 
 
-		q[putloc++] = ch;
+		q[putloc++] = ch; // взяли и засунули, указатель сместили на следующий элемент
 
-		if (putloc == q.length) putloc = 0;
-		if (putloc == getloc) {
-			getloc++;
-			if (getloc == q.length) getloc = 0;
-		}
+		if (putloc == q.length) putloc = 0; // если указатель вышел за пределы массива,
+											// перекинули его на начало массива
+											// зациклили очередь
+
+		if (putloc == getloc) {		// если обнаружили начало очереди, 
+			getloc++;				// то сместили его вперёд
+			if (getloc == q.length) getloc = 0; // если начало очереди вышло за пределы  
+		}								// массива, то зациклили его на начало массива
 	}
 
 	char get() {
-		if (getloc == q.length) getloc = 0;
+		if (getloc == q.length) getloc = 0;	// если указатель на элемент выходит за максимум 
+											// массива то он указывает на начало массива.
 
-		if(getloc == putloc) {
+		if(getloc == putloc) {	// если начало и конец очереди совпадают, то очередь пуста
 			System.out.println(" - Очередь пуста");
 			return (char) 0;
 		}
 
-		return q[getloc++];
+		return q[getloc++]; // читаем по очереди, пока не дочитаем до конца очереди
 	}
 }
 
