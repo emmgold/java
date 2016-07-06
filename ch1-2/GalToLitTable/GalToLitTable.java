54e1ebeb3220cbbc2d0c2773c8bd548ea9d2ce47
user <user@debian.home>
2016-07-06 21:55:28 +0500

    Чуток поигрался

--- ch1-2/GalToLitTable/GalToLitTable.java
+++ ch1-2/GalToLitTable/GalToLitTable.java
@@ -4,7 +4,7 @@ class GalToLitTable {
 
 		for(gallons = 0; gallons <= 100; gallons++) {
 			liters = gallons * 3.7854;
-			System.out.println(gallons + " галлонам соответствует " + liters + " литров");
+			System.out.format("%.3f галлонам соответствует %.3f литров%n", gallons, liters);
 			if(gallons % 10 == 0)
 				System.out.println();
 		}
