4362e6e78bd5d3c270c92a7e39534b09914a3176
EmmGold <emmgold@mail.ru>
2016-07-16 01:31:32 +0500

    Переписал QDemo; Линейная очередь

--- ch5/QDemo/QDemo.java
+++ ch5/QDemo/QDemo.java
@@ -1,67 +1,54 @@
-// Класс, реализующий очередь для хранения символов
-// дополнил, теперь это циклическая очередь
-
 class Queue {
 	char q[];
 	int putloc, getloc;
 
-	Queue (int size) {
-		q = new char[size+1];	// создаём массив на 1 больше нужного, чтобы обработать
-		putloc = getloc = 0;	// начальный момент, когда начало и конец указывают 
-	}							// на один и тот же элемент
+	Queue(int size) {
+		q = new char[size+1];
+		putloc = getloc = 0;
+	}
 
-	void put(char ch) {	// засовываем новый элемент в циклическую очередь
+	void put(char ch) {
+		if(putloc == q.length - 1) {
+			System.out.println(" - Очередь заполнена");
+			return;
+		}
 
-
-		q[putloc++] = ch; // взяли и засунули, указатель сместили на следующий элемент
-
-		if (putloc == q.length) putloc = 0; // если указатель вышел за пределы массива,
-											// перекинули его на начало массива
-											// зациклили очередь
-
-		if (putloc == getloc) {		// если обнаружили начало очереди, 
-			getloc++;				// то сместили его вперёд
-			if (getloc == q.length) getloc = 0; // если начало очереди вышло за пределы  
-		}								// массива, то зациклили его на начало массива
+		putloc++;
+		q[putloc] = ch;
 	}
 
 	char get() {
-		if (getloc == q.length) getloc = 0;	// если указатель на элемент выходит за максимум 
-											// массива то он указывает на начало массива.
-
-		if(getloc == putloc) {	// если начало и конец очереди совпадают, то очередь пуста
+		if(getloc == putloc) {
 			System.out.println(" - Очередь пуста");
 			return (char) 0;
 		}
 
-		return q[getloc++]; // читаем по очереди, пока не дочитаем до конца очереди
+		getloc++;
+		return q[getloc];
 	}
 }
 
 class QDemo {
-	public static void main(String args[]) {
+	public static void main (String args[]) {
 		Queue bigQ = new Queue(100);
-		Queue smallQ = new Queue(6);
-
+		Queue smallQ = new Queue(4);
 		char ch;
 		int i;
 
-		System.out.println("использование очереди bigQ для сохранения алфавита");
-
-		for(i = 0; i < 26; i++)
+		System.out.println("Использование очереди bigQ для сохранения алфавита");
+		for (i = 0; i < 26; i++)
 			bigQ.put((char) ('A' + i));
 
-		System.out.print("Содержимое очереди bigQ: ");
-		for(i = 0; i < 26; i++) {
+		System.out.print("Содержимое очереди bigQ ");
+		for (i = 0; i < 26; i++) {
 			ch = bigQ.get();
 			if(ch != (char) 0) System.out.print(ch);
 		}
 
 		System.out.println("\n");
 
-		System.out.println("использование очереди smallQ для генерации ошибок");
-
-		for(i = 0; i < 8; i++) {
+		System.out.println("Использование очереди smallQ для генерации ошибок");
+		for (i = 0; i < 5; i++) {
 			System.out.print("Попытка сохранения " + (char) ('Z' - i));
 
 			smallQ.put((char) ('Z' - i));
@@ -72,9 +59,11 @@ class QDemo {
 		System.out.println();
 
 		System.out.print("Содержимое smallQ: ");
-		for(i = 0; i < 8; i++) {
+		for(i = 0; i < 5; i++) {
 			ch = smallQ.get();
+
 			if(ch != (char) 0) System.out.print(ch);
 		}
 	}
 }
+
